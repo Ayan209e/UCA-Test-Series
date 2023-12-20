55dd768fca855825c508f6dd64326b963caadb46
Ayan Bishnoi <102575458+Ayan209e@users.noreply.github.com>
2023-12-20 10:58:44 +0000

    Bits & Bytes

---
+++ 01_Bits & Bytes/09_Base64Encoding.c
@@ -0,0 +1,5 @@
+// To ship data across a network various encoding formats are used; one such router in Cisco gets data in Base32 encoded 
+// format. The router has to then transfer the data, converting it to Base64.
+// Your task is to write a code that converts Base32 encoded strings to Base64.
+
+// Pending - To be done and revised
